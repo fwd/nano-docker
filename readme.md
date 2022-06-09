1f5bac3a6c62a06102b0c361e41114cae39b3e8b
Esteban <esteban@fwd.dev>
2022-06-09 18:46:12 -0400

    init

---
+++ readme.md
@@ -0,0 +1,88 @@
+<p align="center">
+  <img src="https://github.com/fwd/n2/raw/master/.github/banner.jpg" alt="Prompts" width="500" />
+</p>
+
+<h2 align="center">Nano Node Docker</h2>
+
+![line](https://github.com/fwd/n2/raw/master/.github/line.png)
+ 
+> ### This software installs a Live Nano Blockchain Node.
+
+![line](https://github.com/fwd/n2/raw/master/.github/line.png)
+
+## ❯ Install
+
+```bash
+curl -L "https://github.com/fwd/nano-docker/raw/master/setup.sh" | sh
+```
+
+```bash
+sudo chmod +x /usr/local/bin/n2
+```
+
+## ❯ Usage
+
+```
+Nano.to
+  $ n2 login
+  $ n2 register
+  $ n2 account
+  $ n2 2factor
+  $ n2 logout
+
+Wallet (Cloud)
+  $ n2 balance
+  $ n2 send @esteban 0.1
+  $ n2 qrcode
+  $ n2 receive
+  $ n2 renew
+
+Blockchain 
+  $ n2 price
+
+Options
+  --help, -h  Print CLI Documentation.
+  --docs, -d  Open Nano.to Documentation.
+  --address, -h  Print you Nano address.
+  --email, -h  Print your account email.
+  --api, -h  Print CLI API KEY email.
+  --update, -u  Get latest CLI Script.
+  --version, -v  Print current CLI Version.
+  --uninstall, -u  Remove CLI from system.
+```
+
+![line](https://github.com/fwd/n2/raw/master/.github/line.png)
+
+## ❯ Contributing
+
+Give a ⭐️ if this project helped you!
+
+Contributions, issues and feature requests are welcome at [issues page](https://github.com/fwd/n2/issues).
+
+## ❯ License
+
+MIT License
+
+Copyright © 2022 [@nano2dev](https://twitter.com/nano2dev).
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## ❯ Stargazers
+
+[![Stargazers over time](https://starchart.cc/fwd/n2.svg)](https://starchart.cc/fwd/n2)
---
+++ setup.sh
@@ -0,0 +1,236 @@
+#!/bin/bash
+
+#################################################
+#  NANO Node Docker                             #
+#  https://github.com/lephleg/nano-node-docker  #
+#################################################
+
+#################################################
+#  Modified by @nano2dev                        #
+#  https://github.com/fwd/nano-docker           #
+#################################################
+
+# VERSION
+version='0.1'
+
+# FAST-SYNC DOWNLOAD LINK
+ledgerDownloadLink="https://s3.us-east-2.amazonaws.com/repo.nano.org/snapshots/Nano_64_2022_05_28_22.7z"
+# ledgerDownloadLink="https://s3.us-east-2.amazonaws.com/repo.nano.org/snapshots/Nano_64_$(date +%Y)_01_20_23.7z"
+
+# OUTPUT VARS
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+bold=`tput bold`
+reset=`tput sgr0`
+
+# FLAGS & ARGUMENTS
+quiet='false'
+displaySeed='false'
+fastSync='false'
+domain=''
+email=''
+tag=''
+while getopts 'sqfd:e:t:' flag; do
+  case "${flag}" in
+    s) displaySeed='true' ;;
+    d) domain="${OPTARG}" ;;
+    e) email="${OPTARG}" ;;
+    q) quiet='true' ;;
+    f) fastSync='true' ;;
+    t) tag="${OPTARG}" ;;
+    *) exit 1 ;;
+  esac
+done
+
+echo $@ > settings
+
+# PRINT INSTALLER DETAILS
+[[ $quiet = 'false' ]] && echo "${green} -----------------------${reset}"
+[[ $quiet = 'false' ]] && echo "${green}${bold} NANO Node Docker ${version}${reset}"
+[[ $quiet = 'false' ]] && echo "${green} -----------------------${reset}"
+[[ $quiet = 'false' ]] && echo ""
+
+# VERIFY TOOLS INSTALLATIONS
+docker -v &> /dev/null
+if [ $? -ne 0 ]; then
+
+    echo "Installing Docker"
+
+    # Docs: https://docs.docker.com/engine/install/ubuntu
+
+    # Basics
+    sudo apt-get -y install curl p7zip-full
+
+    # PGP Key
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+    # Set up Docker Repo
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    
+    sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+fi
+
+docker-compose --version &> /dev/null
+if [ $? -ne 0 ]; then
+    echo "Installing Docker Compose"
+    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+fi
+
+if [[ $tag == '' ]]; then
+    echo "${yellow}Nano node image tag is now required. Please set the -t argument explicitly to the version you are willing to install (https://hub.docker.com/r/nanocurrency/nano/tags).${reset}"
+    exit 2
+fi
+
+if [[ $fastSync = 'true' ]]; then
+    wget --version &> /dev/null
+    if [ $? -ne 0 ]; then
+        echo "${red}wget is not installed and is required for fast-syncing.${reset}";
+        exit 2
+    fi
+
+    7z &> /dev/null
+    if [ $? -ne 0 ]; then
+        echo "${red}7-Zip is not installed and is required for fast-syncing.${reset}";
+        exit 2
+    fi
+fi
+
+# FAST-SYNCING
+if [[ $fastSync = 'true' ]]; then
+
+    if [[ $quiet = 'false' ]]; then
+        printf "=> ${yellow}Downloading latest ledger files for fast-syncing...${reset}\n"
+        wget -O todaysledger.7z ${ledgerDownloadLink} -q --show-progress
+
+        printf "=> ${yellow}Unzipping and placing the files (takes a while)...${reset} "
+        7z x todaysledger.7z  -o./nano-node/Nano -y &> /dev/null
+        rm todaysledger.7z
+        printf "${green}done.${reset}\n"
+        echo ""
+
+    else
+        wget -O todaysledger.7z ${ledgerDownloadLink} -q
+        docker-compose stop nano-node &> /dev/null
+        7z x todaysledger.7z  -o./nano-node/Nano -y &> /dev/null
+        rm todaysledger.7z
+    fi
+
+fi
+
+# DETERMINE IF THIS IS AN INITIAL INSTALL
+[[ $quiet = 'false' ]] && echo "=> ${yellow}Checking initial status...${reset}"
+[[ $quiet = 'false' ]] && echo ""
+
+# check if node mounted directory exists
+if [ -d "./nano-node" ]; then
+    # check if mounted directory follows the new /root structure
+    if [ ! -d "./nano-node/RaiBlocks" ]; then
+        if [ ! -d "./nano-node/Nano" ]; then
+            [[ $quiet = 'false' ]] && printf "${reset}Unsupported directory structure detected. Migrating files... "
+            mkdir ./nano-node/RaiBlocks
+            # move everything into subdirectory and suppress the error about itself
+            mv ./nano-node/* ./nano-node/RaiBlocks/ &> /dev/null
+            [[ $quiet = 'false' ]] && printf "${green}done.\n${reset}"
+            [[ $quiet = 'false' ]] && echo ""
+        fi
+    fi
+fi
+
+# SPIN UP THE APPROPRIATE STACK
+[[ $quiet = 'false' ]] && echo "=> ${yellow}Pulling images and spinning up containers...${reset}"
+[[ $quiet = 'false' ]] && echo ""
+
+docker network create nano-node-network &> /dev/null
+
+if [[ $tag ]]; then
+    sed -i -e "s/    image: nanocurrency\/nano:.*/    image: nanocurrency\/nano:$tag/g" docker-compose.yml
+fi
+
+if [[ $quiet = 'false' ]]; then
+    docker-compose up -d
+else
+    docker-compose up -d &> /dev/null
+fi
+
+if [ $? -ne 0 ]; then
+    echo "${red}It seems errors were encountered while spinning up the containers. Scroll up for more info on how to fix them.${reset}"
+    exit 2
+fi
+
+# CHECK NODE INITIALIZATION
+[[ $quiet = 'false' ]] && echo ""
+[[ $quiet = 'false' ]] && printf "=> ${yellow}Waiting for NANO node to fully initialize... "
+
+isRpcLive="$(curl -s -d '{"action": "version"}' [::1]:7076 | grep "rpc_version")"
+while [ ! -n "$isRpcLive" ];
+do
+    sleep 1s
+    isRpcLive="$(curl -s -d '{"action": "version"}' [::1]:7076 | grep "rpc_version")"
+done
+
+[[ $quiet = 'false' ]] && printf "${green}done.${reset}\n\n"
+
+# DETERMINE NODE VERSION
+nodeExec="docker exec -it nano-node /usr/bin/nano_node"
+
+# SET BASH ALIASES FOR NODE CLI
+if [ -f ~/.bash_aliases ]; then
+    alias=$(cat ~/.bash_aliases | grep 'nano-node');
+    if [[ ! $alias ]]; then
+        echo "alias nano-node='${nodeExec}'" >> ~/.bash_aliases;
+        source ~/.bashrc;
+    fi
+else
+    echo "alias nano-node='${nodeExec}'" >> ~/.bash_aliases;
+    source ~/.bashrc;
+fi
+
+# WALLET SETUP
+
+sed -i 's/enable_control = false/enable_control = true/g' ~/nano-docker/nano-node/Nano/config-rpc.toml
+
+existedWallet="$(${nodeExec} --wallet_list | grep 'Wallet ID' | awk '{ print $NF}')"
+
+if [[ ! $existedWallet ]]; then
+    [[ $quiet = 'false' ]] && printf "=> ${yellow}No wallet found. Generating a new one... ${reset}"
+
+    walletId=$(${nodeExec} --wallet_create | tr -d '\r')
+    address="$(${nodeExec} --account_create --wallet=$walletId | awk '{ print $NF}')"
+    
+    [[ $quiet = 'false' ]] && printf "${green}done.${reset}\n\n"
+else
+    [[ $quiet = 'false' ]] && echo "=> ${yellow}Existing wallet found.${reset}"
+    [[ $quiet = 'false' ]] && echo ''
+
+    address="$(${nodeExec} --wallet_list | grep 'nano_' | awk '{ print $NF}' | tr -d '\r')"
+    walletId=$(echo $existedWallet | tr -d '\r')
+fi
+
+if [[ $quiet = 'false' && $displaySeed = 'true' ]]; then
+    seed=$(${nodeExec} --wallet_decrypt_unsafe --wallet=$walletId | grep 'Seed' | awk '{ print $NF}' | tr -d '\r')
+fi
+
+if [[ $quiet = 'false' ]]; then
+
+    echo "${yellow} |=========================================================================================| ${reset}"
+    echo "${yellow} | ${green}${bold}Congratulations! NANO Node Docker has been setup successfully!                          ${yellow}| ${reset}"
+    echo "${yellow} |=========================================================================================| ${reset}"
+    echo "${yellow} | Node account address: ${green}$address${yellow} | ${reset}"
+    if [[ $displaySeed = 'true' ]]; then
+        echo "${yellow} | Node wallet seed: ${red}$seed${yellow}      | ${reset}"
+    fi
+    echo "${yellow} |=========================================================================================| ${reset}"
+
+    echo ""
+
+    echo "${yellow} Node is now active: 'https://localhost:7676' or '[::1]:7676'"
+    
+    echo ""
+
+fi
