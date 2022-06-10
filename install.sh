ff5258dcfd507022e654d10feeac21759a9af3d4
Esteban <esteban@fwd.dev>
2022-06-09 23:34:53 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 cd ~
 git clone https://github.com/fwd/nano-docker.git
 cd ~/nano-docker
-LATEST=$(curl -sL https://api.github.com/repos/nanocurrency/nano-node/releases/latest | jq -r ".tag_name")
-sudo ./setup.sh -s -t $LATEST
+sudo ./setup.sh -s -t 23.1
