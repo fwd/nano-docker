4cc37c8d02a2a90c169a96fc1d715f81f43d7420
Esteban <esteban@fwd.dev>
2022-06-09 23:38:37 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -70,6 +70,8 @@ if [ $? -ne 0 ]; then
       "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
       $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     
+    sudo apt-get update
+    
     sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 fi
