967fbfb3c11b60fc55b32bc06894c93fdb2439d4
Esteban <esteban@fwd.dev>
2022-06-22 15:59:42 -0400

    flags

--- setup.sh
+++ setup.sh
@@ -13,11 +13,6 @@
 # VERSION
 version='0.2'
 
-# FAST-SYNC DOWNLOAD LINK
-# BIG THANKS TO @ThiagoSFS
-ledgerDownloadLink=$(curl 'https://s3.us-east-2.amazonaws.com/repo.nano.org/snapshots/latest')
-# ledgerDownloadLink="https://s3.us-east-2.amazonaws.com/repo.nano.org/snapshots/Nano_64_2022_05_28_22.7z"
-
 # OUTPUT VARS
 red=`tput setaf 1`
 green=`tput setaf 2`
@@ -25,25 +20,53 @@ yellow=`tput setaf 3`
 bold=`tput bold`
 reset=`tput sgr0`
 
-# FLAGS & ARGUMENTS
+if [[ "$1" == "-q" ]] || [[ "$2" == "-q" ]] || [[ "$3" == "-q" ]] || [[ "$4" == "-q" ]] || [[ "$5" == "-q" ]] || [[ "$6" == "-q" ]] || [[ "$7" == "-q" ]]; then
+quiet='true'
+else
 quiet='false'
-displaySeed='false'
+fi
+
+if [[ "$1" == "-f" ]] || [[ "$2" == "-f" ]] || [[ "$3" == "-f" ]] || [[ "$4" == "-f" ]] || [[ "$5" == "-f" ]] || [[ "$6" == "-f" ]] || [[ "$7" == "-f" ]]; then
+fastSync='true'
+else
 fastSync='false'
-domain=''
-email=''
-tag=''
-while getopts 'sqfd:e:t:' flag; do
-  case "${flag}" in
-    s) displaySeed='true' ;;
-    d) domain="${OPTARG}" ;;
-    e) email="${OPTARG}" ;;
-    q) quiet='true' ;;
-    f) fastSync='true' ;;
-    t) tag="${OPTARG}" ;;
-    v) tag="${OPTARG}" ;;
-    *) exit 1 ;;
-  esac
-done
+fi
+
+if [[ "$1" == "-m" ]] || [[ "$2" == "-m" ]] || [[ "$3" == "-m" ]] || [[ "$4" == "-m" ]] || [[ "$5" == "-m" ]] || [[ "$6" == "-m" ]] || [[ "$7" == "-m" ]]; then
+monitor='true'
+else
+monitor='false'
+fi
+
+if [[ "$1" == "-t" ]] || [[ "$2" == "-t" ]] || [[ "$3" == "-t" ]] || [[ "$4" == "-t" ]] || [[ "$5" == "-v" ]] || [[ "$6" == "-v" ]] || [[ "$7" == "-v" ]]; then
+    if [[ "$2" == "-t" ]] || [[ "$2" == "-v" ]]; then tag=$3; fi;
+    if [[ "$3" == "-t" ]] || [[ "$3" == "-v" ]]; then tag=$4; fi;
+    if [[ "$4" == "-t" ]] || [[ "$4" == "-v" ]]; then tag=$5; fi;
+    if [[ "$5" == "-t" ]] || [[ "$5" == "-v" ]]; then tag=$6; fi;
+    if [[ "$7" == "-t" ]] || [[ "$7" == "-v" ]]; then tag=$8; fi;
+fi
+
+if [[ "$1" == "-p" ]] || [[ "$2" == "-p" ]] || [[ "$3" == "-p" ]] || [[ "$4" == "-p" ]] || [[ "$5" == "-p" ]] || [[ "$6" == "-p" ]] || [[ "$7" == "-p" ]]; then
+    if [[ "$2" == "-p" ]] || [[ "$2" == "-port" ]]; then port=$3; fi;
+    if [[ "$3" == "-p" ]] || [[ "$3" == "-port" ]]; then port=$4; fi;
+    if [[ "$4" == "-p" ]] || [[ "$4" == "-port" ]]; then port=$5; fi;
+    if [[ "$5" == "-p" ]] || [[ "$5" == "-port" ]]; then port=$6; fi;
+    if [[ "$6" == "-p" ]] || [[ "$6" == "-port" ]]; then port=$7; fi;
+    if [[ "$7" == "-p" ]] || [[ "$7" == "-port" ]]; then port=$8; fi;
+    if [[ "$8" == "-p" ]] || [[ "$8" == "-port" ]]; then port=$9; fi;
+fi
+
+if [[ -z $port ]]; then port='80'; fi
+
+if [[ -z $tag ]]; then tag=$(curl -s https://api.github.com/repos/nanocurrency/nano-node/releases/latest -s | jq .name -r); fi
+
+# Development
+# echo "quiet", $quiet
+# echo "fastSync", $fastSync
+# echo "monitor", $monitor
+# echo "tag", $tag
+# echo "port", $port
+# exit 1
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     echo ""
@@ -58,9 +81,9 @@ elif [[ "$OSTYPE" == "cygwin" ]]; then
 elif [[ "$OSTYPE" == "msys" ]]; then
     echo "Operating system not supported."
     exit 1
-  # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+    # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
 elif [[ "$OSTYPE" == "win32" ]]; then
-  # I'm not sure this can happen.
+    # I'm not sure this can happen.
     echo "Operating system not supported."
     exit 1
 elif [[ "$OSTYPE" == "freebsd"* ]]; then
@@ -75,14 +98,79 @@ fi
 
 echo $@ > settings
 
+# ================================
+# This is either genius or dumb. 
+# ================================
+# Time will tell.
+DEFAULT_COMPOSE=$(cat <<EOF
+version: '3'
+services:
+  nano-node:
+    image: nanocurrency/nano:latest
+    container_name: nano-node
+    hostname: nano-node
+    environment:
+      - TERM=xterm
+    restart: unless-stopped
+    ports:
+      - "7075:7075/udp"
+      - "7075:7075"
+      - "::1:7076:7076"
+      - "::1:7078:7078"
+    volumes:
+      - ./nano-node:/root
+  watchtower:
+    image: v2tec/watchtower
+    container_name: watchtower
+    restart: unless-stopped
+    command: watchtower nano-node nano-node-monitor
+    volumes:
+      - /var/run/docker.sock:/var/run/docker.sock
+EOF
+)
+
+DOCKER_MONITOR_COMPOSE=$(cat <<EOF
+  nano-node-monitor:
+    image: nanotools/nanonodemonitor
+    container_name: nano-node-monitor
+    hostname: nano-node-monitor
+    restart: unless-stopped
+    volumes:
+      - ./nano-node-monitor:/opt/nanoNodeMonitor
+    ports:
+      - "${port}:80"
+EOF
+)
+
+if [ $monitor == "true" ]; then
+FINAL_COMPOSE_FILE=$(cat <<EOF
+${DEFAULT_COMPOSE}
+${DOCKER_MONITOR_COMPOSE}
+EOF
+)
+else
+FINAL_COMPOSE_FILE=$(cat <<EOF
+${DEFAULT_COMPOSE}
+EOF
+)
+fi
+
+cat > _docker-compose.yml <<EOF
+$FINAL_COMPOSE_FILE
+EOF
+
+# exit 1
+
 # PRINT INSTALLER DETAILS
 [[ $quiet = 'false' ]] && echo "${green}=================================${reset}"
 [[ $quiet = 'false' ]] && echo "${green}${bold}1-CLICK NANO NODE ${reset}"
 [[ $quiet = 'false' ]] && echo "${green}=================================${reset}"
-[[ $quiet = 'false' ]] && echo "${green}${bold}by Nano.to ${reset}"
+[[ $quiet = 'false' ]] && echo "${green}${bold}by @nano2dev${reset}"
 [[ $quiet = 'false' ]] && echo "${green}=================================${reset}"
 [[ $quiet = 'false' ]] && echo ""
 
+# Calm before storm
+sleep 2
 
 if ! command -v jq &> /dev/null; then
     if [  -n "$(uname -a | grep Ubuntu)" ]; then
@@ -126,11 +214,6 @@ if [ $? -ne 0 ]; then
     sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 fi
 
-# if [[ $tag == '' ]]; then
-#     echo "${yellow}Nano node image tag is now required. Please set the -t argument explicitly to the version you are willing to install (https://hub.docker.com/r/nanocurrency/nano/tags).${reset}"
-#     exit 2
-# fi
-
 if [[ $fastSync = 'true' ]]; then
     wget --version &> /dev/null
     if [ $? -ne 0 ]; then
@@ -147,6 +230,10 @@ fi
 
 # FAST-SYNCING
 if [[ $fastSync = 'true' ]]; then
+
+    # FAST-SYNC DOWNLOAD LINK
+    # BIG THANKS TO @ThiagoSFS
+    ledgerDownloadLink=$(curl -s 'https://s3.us-east-2.amazonaws.com/repo.nano.org/snapshots/latest')
 
     if [[ $quiet = 'false' ]]; then
         printf "=> ${yellow}Downloading latest ledger files for fast-syncing...${reset}\n"
@@ -192,10 +279,46 @@ fi
 
 docker network create nano-node-network &> /dev/null
 
-TAG=$(curl https://api.github.com/repos/nanocurrency/nano-node/releases/latest -s | jq .name -r)
+if [ $monitor != '' ]; then
 
-if [[ $TAG ]]; then
-    sed -i -e "s/    image: nanocurrency\/nano:.*/    image: nanocurrency\/nano:$TAG/g" docker-compose.yml
+    # UPDATE MONITOR CONFIGS
+    if [ ! -f ./nano-node-monitor/config.php ]; then
+        [[ $quiet = 'false' ]] && echo "=> ${yellow}No existing NANO Node Monitor config file found. Fetching a fresh copy...${reset}"
+        if [[ $quiet = 'false' ]]; then
+            docker-compose restart nano-node-monitor
+        else
+            docker-compose restart nano-node-monitor > /dev/null
+        fi
+    fi
+
+    [[ $quiet = 'false' ]] && printf "=> ${yellow}Configuring NANO Node Monitor... ${reset}"
+
+    sed -i -e "s/\/\/ \$nanoNodeRPCIP.*;/\$nanoNodeRPCIP/g" ./nano-node-monitor/config.php
+    sed -i -e "s/\$nanoNodeRPCIP.*/\$nanoNodeRPCIP = 'nano-node';/g" ./nano-node-monitor/config.php
+
+    sed -i -e "s/\/\/ \$nanoNodeAccount.*;/\$nanoNodeAccount/g" ./nano-node-monitor/config.php
+    sed -i -e "s/\$nanoNodeAccount.*/\$nanoNodeAccount = '$address';/g" ./nano-node-monitor/config.php
+
+    ipAddress=$(curl -s v4.ifconfig.co | awk '{ print $NF}' | tr -d '\r')
+
+    # in case of an ipv6 address, add square brackets
+    if [[ $ipAddress =~ .*:.* ]]; then
+        ipAddress="[$ipAddress]"
+    fi
+
+    sed -i -e "s/\/\/ \$nanoNodeName.*;/\$nanoNodeName = 'nano-node-docker-$ipAddress';/g" ./nano-node-monitor/config.php
+
+    sed -i -e "s/\/\/ \$welcomeMsg.*;/\$welcomeMsg = 'Welcome! This node was setup using <a href=\"https:\/\/github.com\/lephleg\/nano-node-docker\" target=\"_blank\">NANO Node Docker<\/a>!';/g" ./nano-node-monitor/config.php
+    
+    sed -i -e "s/\/\/ \$blockExplorer.*;/\$blockExplorer = 'meltingice';/g" ./nano-node-monitor/config.php
+
+    sed -i -e 's/\r//g' ./nano-node-monitor/config.php
+
+fi
+
+
+if [[ $tag ]]; then
+    sed -i -e "s/    image: nanocurrency\/nano:.*/    image: nanocurrency\/nano:$tag/g" docker-compose.yml
 fi
 
 if [[ $quiet = 'false' ]]; then
@@ -251,7 +374,7 @@ $ nano-node --help
 Curl: 
 $ curl -g -d '{ "action": "telemetry" }' '[::1]:7076'   
 
-You might need to run: source ~/.bash_aliases
+You might need to: source ~/.bash_aliases
 EOF
 
 # At the end for good luck.
